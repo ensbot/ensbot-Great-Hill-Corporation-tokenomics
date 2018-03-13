ae88be4009146ce5bc341de121a58060f1cd3e51
emazurek <changes@gmail.com>
2018-03-13 17:04:58 -0400

    fix migration, add seed scaffold

--- api/v0/src/server/db/migrations/20180313133633_transactions.js
+++ api/v0/src/server/db/migrations/20180313133633_transactions.js
@@ -8,9 +8,9 @@ exports.up = (knex, Promise) => {
     table.string('to', 42).notNullable();
     table.bigInteger('value').unsigned().notNullable().defaultTo(0); // max val: 	2^64-1
     table.integer('gasCost').unsigned().notNullable();
-    table.boolean('isError').notNullable().defaultTo(F);
-    // table.boolean('isFnCall').notNullable().defaultTo(F);
-    table.boolean('isFinalized').notNullable().defaultTo(F);
+    table.boolean('isError').notNullable().defaultTo(false);
+    // table.boolean('isFnCall').notNullable().defaultTo(false);
+    table.boolean('isFinalized').notNullable().defaultTo(false);
     table.text('articulated', 'longtext');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
