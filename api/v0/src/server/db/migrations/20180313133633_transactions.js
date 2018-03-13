65812f318b50624b12934b83d33acc0073e11c71
emazurek <changes@gmail.com>
2018-03-13 16:57:34 -0400

    define transactions table

--- api/v0/src/server/db/migrations/20180313133633_transactions.js
+++ api/v0/src/server/db/migrations/20180313133633_transactions.js
@@ -1,8 +1,17 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('transactions', (table) => {
     table.increments();
-    table.string('username').unique().notNullable();
-    table.string('email').unique().notNullable();
+    table.dateTime('dateTime').notNullable();
+    table.integer('blockNumber').unsigned().notNullable(); // max val: 4294967295
+    table.integer('transactionIndex').unsigned().notNullable();
+    table.string('from', 42).notNullable();
+    table.string('to', 42).notNullable();
+    table.bigInteger('value').unsigned().notNullable().defaultTo(0); // max val: 	2^64-1
+    table.integer('gasCost').unsigned().notNullable();
+    table.boolean('isError').notNullable().defaultTo(F);
+    // table.boolean('isFnCall').notNullable().defaultTo(F);
+    table.boolean('isFinalized').notNullable().defaultTo(F);
+    table.text('articulated', 'longtext');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
