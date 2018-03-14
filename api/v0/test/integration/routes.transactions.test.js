195abca46d3644dbb0dd0ad266cde121942187e7
emazurek <changes@gmail.com>
2018-03-14 12:08:49 -0400

    limit db seed to 10 rows

--- api/v0/test/integration/routes.transactions.test.js
+++ api/v0/test/integration/routes.transactions.test.js
@@ -39,8 +39,8 @@ describe('routes : transactions', () => {
         // key-value pair of {"status": "success"}
         res.body.status.should.eql('success');
         // the JSON response body should have a
-        // key-value pair of {"data": [2 user objects]}
-        res.body.data.length.should.eql(2);
+        // key-value pair of {"data": [10 transaction objects]}
+        res.body.data.length.should.eql(10);
         // the first object in the data array should
         // have the right keys
         res.body.data[0].should.include.keys('id', 'dateTime', 'blockNumber', 'transactionIndex', 'from', 'to', 'value', 'gasCost', 'isError', 'isFinalized', 'articulated', 'created_at',);
