ff9e378d656332723dfe257bb63bdfe73cfd41c1
emazurek <changes@gmail.com>
2018-03-14 11:41:58 -0400

    database seed reads from etherTip example file

--- api/v0/src/server/db/migrations/20180313133633_transactions.js
+++ api/v0/src/server/db/migrations/20180313133633_transactions.js
@@ -1,13 +1,15 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('transactions', (table) => {
+    table.charset('utf8mb4');
+    table.collate('utf8mb4_bin');
     table.increments();
-    table.dateTime('dateTime').notNullable();
+    table.datetime('dateTime').notNullable();
     table.integer('blockNumber').unsigned().notNullable(); // max val: 4294967295
     table.integer('transactionIndex').unsigned().notNullable();
     table.string('from', 42).notNullable();
     table.string('to', 42).notNullable();
     table.bigInteger('value').unsigned().notNullable().defaultTo(0); // max val: 	2^64-1
-    table.integer('gasCost').unsigned().notNullable();
+    table.decimal('gasCost', 21, 18).unsigned().notNullable(); // expects that gasCost will never be more specific than XXX.XXXXXXXXXXXXXXXXXX
     table.boolean('isError').notNullable().defaultTo(false);
     // table.boolean('isFnCall').notNullable().defaultTo(false);
     table.boolean('isFinalized').notNullable().defaultTo(false);
--- api/v0/src/server/db/seeds/transactions.js
+++ api/v0/src/server/db/seeds/transactions.js
@@ -1,13 +1,38 @@
+const fs = require('fs');
+const csv = require('csv');
+const util = require('util');
+const csvParse = util.promisify(csv.parse);
+const readFile = util.promisify(fs.readFile);
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+ return knex('transactions').del()
+   .then(() => {
+     return readFile('../../monitors/etherTip_data.txt')
+       .then((res) => {
+        return csvParse(res, {delimiter: '\t',
+                       relax: true,
+                       columns: true})
+        })
+    }).then((res) => {
+      const dataToInsert = res.map((row) => {
+        return {
+          dateTime: row.DATESH + ' ' + row.TIME.slice(0, -4),
+          blockNumber: row.BLOCKNUMBER,
+          transactionIndex: row.TRANSACTIONINDEX,
+          from: row.FROM,
+          to: row.TO,
+          value: row.ETHER,
+          gasCost: row.GASCOST,
+          articulated: row.ARTICULATED
+        }
+      });
+     return Promise.all([
+       // Inserts seed entries
+       knex('transactions').insert(dataToInsert),
+     ])
+   })
+     .catch(e => {
+       return console.log(e)
+     });
 };
