e1e5ce36992517437bd3757fcaef4df1232e691d
emazurek <changes@gmail.com>
2018-03-14 12:00:05 -0400

    update route config

--- api/v0/src/server/config/route-config.js
+++ api/v0/src/server/config/route-config.js
@@ -6,10 +6,11 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const transactionRoutes = require('../routes/transactions');
 
     // *** register routes *** //
     app.use('/', routes);
-
+    app.use('/api/v0/transactions', transactionRoutes);
   };
 
 })(module.exports);
