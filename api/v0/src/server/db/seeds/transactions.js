912c440419709d500f4fadcd63a1489cd31540cf
emazurek <changes@gmail.com>
2018-03-14 11:58:47 -0400

    add GET ALL Transactions route test, watch it fail

--- api/v0/src/server/db/seeds/transactions.js
+++ api/v0/src/server/db/seeds/transactions.js
@@ -6,33 +6,30 @@ const readFile = util.promisify(fs.readFile);
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
- return knex('transactions').del()
-   .then(() => {
-     return readFile('../../monitors/etherTip_data.txt')
-       .then((res) => {
-        return csvParse(res, {delimiter: '\t',
-                       relax: true,
-                       columns: true})
-        })
-    }).then((res) => {
-      const dataToInsert = res.map((row) => {
-        return {
-          dateTime: row.DATESH + ' ' + row.TIME.slice(0, -4),
-          blockNumber: row.BLOCKNUMBER,
-          transactionIndex: row.TRANSACTIONINDEX,
-          from: row.FROM,
-          to: row.TO,
-          value: row.ETHER,
-          gasCost: row.GASCOST,
-          articulated: row.ARTICULATED
-        }
-      });
-     return Promise.all([
-       // Inserts seed entries
-       knex('transactions').insert(dataToInsert),
-     ])
-   })
-     .catch(e => {
-       return console.log(e)
-     });
+  return knex('transactions').del().then(() => {
+    return readFile('../../monitors/etherTip_data.txt').then((res) => {
+      return csvParse(res, {
+        delimiter: '\t',
+        relax: true,
+        columns: true
+      })
+    })
+  }).then((res) => {
+    const dataToInsert = res.map((row) => {
+      return {
+        dateTime: row.DATESH + ' ' + row.TIME.slice(0, -4),
+        blockNumber: row.BLOCKNUMBER,
+        transactionIndex: row.TRANSACTIONINDEX,
+        from: row.FROM,
+        to: row.TO,
+        value: row.ETHER,
+        gasCost: row.GASCOST,
+        articulated: row.ARTICULATED
+      }
+    });
+    return Promise.all([// Inserts seed entries
+      knex('transactions').insert(dataToInsert)])
+  }).catch(e => {
+    return console.log(e)
+  });
 };
--- api/v0/test/integration/routes.transactions.test.js
+++ api/v0/test/integration/routes.transactions.test.js
@@ -6,17 +6,14 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 const server = require('../../src/server/app');
-const knex = require('../../src/server/db/knex');
+const knex = require('../../src/server/db/connection');
 
 describe('routes : transactions', () => {
 
   beforeEach((done) => {
-    knex.migrate.rollback()
-    .then(() => {
-      knex.migrate.latest()
-      .then(() => {
-        knex.seed.run()
-        .then(() => {
+    knex.migrate.rollback().then(() => {
+      knex.migrate.latest().then(() => {
+        knex.seed.run().then(() => {
           done();
         })
       });
@@ -24,9 +21,31 @@ describe('routes : transactions', () => {
   });
 
   afterEach((done) => {
-    knex.migrate.rollback()
-    .then(() => {
+    knex.migrate.rollback().then(() => {
       done();
+    });
+  });
+
+  describe('GET /api/v0/transactions', () => {
+    it('should respond with all transactions', (done) => {
+      chai.request(server).get('/api/v0/transactions').end((err, res) => {
+        // there should be no errors
+        should.not.exist(err);
+        // there should be a 200 status code
+        res.status.should.equal(200);
+        // the response should be JSON
+        res.type.should.equal('application/json');
+        // the JSON response body should have a
+        // key-value pair of {"status": "success"}
+        res.body.status.should.eql('success');
+        // the JSON response body should have a
+        // key-value pair of {"data": [2 user objects]}
+        res.body.data.length.should.eql(2);
+        // the first object in the data array should
+        // have the right keys
+        res.body.data[0].should.include.keys('id', 'dateTime', 'blockNumber', 'transactionIndex', 'from', 'to', 'value', 'gasCost', 'isError', 'isFinalized', 'articulated', 'created_at',);
+        done();
+      });
     });
   });
 
