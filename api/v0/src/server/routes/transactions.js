2d0b680df0f9585d52e41dc09272ce1b5c8fd50b
emazurek <changes@gmail.com>
2018-03-14 22:25:03 -0400

    add POST Transaction route, fix test

--- api/v0/src/server/routes/transactions.js
+++ api/v0/src/server/routes/transactions.js
@@ -4,18 +4,29 @@ const router = express.Router();
 const knex = require('../db/connection');
 
 router.get('/', (req, res, next) => {
-  knex('transactions').select('*')
-  .then((transactions) => {
-    res.status(200).json({
-      status: 'success',
-      data: transactions
-    });
-  })
-  .catch((err) => {
-    res.status(500).json({
-      status: 'error',
-      data: err
-    });
+  knex('transactions').select('*').then((transactions) => {
+    res.status(200).json({status: 'success', data: transactions});
+  }).catch((err) => {
+    res.status(500).json({status: 'error', data: err});
+  });
+});
+
+router.post('/', (req, res, next) => {
+  knex('transactions').insert({
+    dateTime: req.body.dateTime,
+    blockNumber: req.body.blockNumber,
+    transactionIndex: req.body.transactionIndex,
+    from: req.body.from,
+    to: req.body.to,
+    value: req.body.value,
+    gasCost: req.body.gasCost,
+    isError: req.body.isError,
+    isFinalized: req.body.isFinalized,
+    articulated: req.body.articulated
+  }).then((transaction) => {
+    res.status(201).json({status: 'success', data: transaction});
+  }).catch((err) => {
+    res.status(500).json({status: 'error', data: err});
   });
 });
 
