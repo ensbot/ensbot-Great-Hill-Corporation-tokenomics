6cd8a876004ce8fe65357d2602b4b02204acc710
emazurek <changes@gmail.com>
2018-03-21 20:46:24 -0400

    API v0: alternative schema

--- api/v0/knexfile.js
+++ api/v0/knexfile.js
@@ -4,6 +4,22 @@ const password = process.env.MYSQL_PW;
 const databaseName = 'tokenomics_test';
 
 module.exports = {
+  production: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: username,
+      password: password,
+      database: databaseName,
+      charset: 'utf8mb4'
+    },
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
   development: {
     client: 'mysql',
     connection: {
--- api/v0/src/server/db/migrations/20180313133633_transactions.js
+++ api/v0/src/server/db/migrations/20180313133633_transactions.js
@@ -1,23 +1,51 @@
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('transactions', (table) => {
-    table.charset('utf8mb4');
-    table.collate('utf8mb4_bin');
-    table.increments();
-    table.datetime('dateTime').notNullable();
-    table.integer('blockNumber').unsigned().notNullable(); // max val: 4294967295
-    table.integer('transactionIndex').unsigned().notNullable();
-    table.string('from', 42).notNullable();
-    table.string('to', 42).notNullable();
-    table.bigInteger('value').unsigned().notNullable().defaultTo(0); // max val: 	2^64-1
-    table.decimal('gasCost', 21, 18).unsigned().notNullable(); // expects that gasCost will never be more specific than XXX.XXXXXXXXXXXXXXXXXX
-    table.boolean('isError').notNullable().defaultTo(false);
-    // table.boolean('isFnCall').notNullable().defaultTo(false);
-    table.boolean('isFinalized').notNullable().defaultTo(false);
-    table.text('articulated', 'longtext');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-  });
+  return knex.schema
+    .createTable('address', (table) => {
+      table.increments('id');
+      table.bigInteger('address').notNullable();
+      table.boolean('is_monitored').notNullable().defaultTo(false);
+      table.boolean('known_contract').notNullable().defaultTo(false);
+      table.string('nickname');
+    })
+    .createTable('transaction', (table) => {
+      table.charset('utf8mb4');
+      table.collate('utf8mb4_bin');
+      table.increments();
+      table.datetime('date_time').notNullable();
+      table.integer('block_number').unsigned().notNullable(); // max val: 4294967295
+      table.integer('transaction_index').unsigned().notNullable();
+      table.integer('trace_id').unsigned().notNullable();
+      table.integer('from', 42).unsigned().references('id').inTable('address').notNullable();
+      table.integer('to', 42).unsigned().references('id').inTable('address').notNullable();
+      table.bigInteger('value_wei').unsigned().notNullable().defaultTo(0); // max val: 	2^64-1
+      table.decimal('gas_cost', 21, 18).unsigned().notNullable(); // expects that gasCost will never be more specific than XXX.XXXXXXXXXXXXXXXXXX
+      table.boolean('is_error').notNullable().defaultTo(false);
+      table.boolean('is_finalized').notNullable().defaultTo(false);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      //table.primary(['block_number', 'transaction_index', 'trace_id']);
+    })
+    .createTable('abi_spec', (table) => {
+      table.increments();
+      table.integer('contract_address_id').unsigned().references('id').inTable('address').notNullable();
+      table.string('fn_name').notNullable();
+      table.string('arg_name').notNullable();
+      table.integer('arg_index').unsigned().notNullable();
+    })
+    .createTable('input_data', (table) => {
+      table.increments();
+      //table.integer('block_number').unsigned().references('block_number').inTable('transaction').notNullable();
+      //table.integer('transaction_index').unsigned().references('transaction_index').inTable('transaction').notNullable();
+      //table.integer('trace_id').unsigned().references('trace_index').inTable('transaction').notNullable();
+      table.integer('transaction_id').unsigned().references('id').inTable('transaction').notNullable();
+      table.integer('abi_id').unsigned().references('id').inTable('abi_spec').notNullable();
+      table.text('arg_value');
+    });
 };
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('transactions');
+  return knex.schema
+    .dropTable('address')
+    .dropTable('transactions')
+    .dropTable('abi_spec')
+    .dropTable('input_data');
 };
