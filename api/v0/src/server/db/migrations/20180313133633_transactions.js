9291f372612362995a083f990afed3ac5f602bf5
emazurek <changes@gmail.com>
2018-03-22 18:02:45 -0400

    change certain db CASCADE behavior to RESTRICT. Move transaction.is_finalized to block.is_finalized

--- api/v0/src/server/db/migrations/20180313133633_transactions.js
+++ api/v0/src/server/db/migrations/20180313133633_transactions.js
@@ -16,8 +16,9 @@ exports.up = (knex, Promise) => {
       table.decimal('exchange_rate', 21, 18).unsigned().notNullable();
     })
     .createTable('block', (table) => {
-      table.integer('block_number').unsigned().primary().notNullable();
+      table.integer('block_number').unsigned().primary().notNullable(); // max val: 4294967295
       table.datetime('date_time').notNullable();
+      table.boolean('is_finalized').notNullable().defaultTo(false);
       table.timestamp('created_at').defaultTo(knex.fn.now());
     })
     // create an address table so that addresses are only stored once
@@ -31,7 +32,8 @@ exports.up = (knex, Promise) => {
     .createTable('transaction', (table) => {
       table.charset('utf8mb4');
       table.collate('utf8mb4_bin');
-      table.integer('block_number').unsigned().references('block_number').inTable('block').notNullable().onDelete('CASCADE'); // max val: 4294967295
+      table.integer('block_number').unsigned().references('block_number').inTable('block').notNullable()
+        .onDelete('CASCADE'); // delete the transaction if the block gets deleted.
       table.integer('tx_index').unsigned().notNullable();
       table.integer('trace_id').unsigned().notNullable();
       table.integer('from', 42).unsigned().references('id').inTable('address').notNullable();
@@ -39,20 +41,22 @@ exports.up = (knex, Promise) => {
       table.bigInteger('value_wei').unsigned().notNullable().defaultTo(0); // max val: 	2^64-1
       table.decimal('gas_cost', 21, 18).unsigned().notNullable(); // expects that gasCost will never be more specific than XXX.XXXXXXXXXXXXXXXXXX
       table.boolean('is_error').notNullable().defaultTo(false);
-      table.boolean('is_finalized').notNullable().defaultTo(false);
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.primary(['block_number', 'tx_index', 'trace_id']);
     })
     .createTable('address_transaction', (table) => {
-      table.integer('address_id').unsigned().references('id').inTable('address').notNullable().onDelete('CASCADE');
+      table.integer('address_id').unsigned().references('id').inTable('address').notNullable()
+        .onDelete('RESTRICT'); // Prevent address deletion if we have a transaction for it
       table.integer('block_number').unsigned().notNullable();
       table.integer('tx_index').unsigned().notNullable();
       table.integer('trace_id').unsigned().notNullable();
-      table.foreign(['block_number', 'tx_index', 'trace_id']).references(['block_number', 'tx_index', 'trace_id']).inTable('transaction').onDelete('CASCADE');
+      table.foreign(['block_number', 'tx_index', 'trace_id']).references(['block_number', 'tx_index', 'trace_id']).inTable('transaction')
+        .onDelete('CASCADE'); // Delete the address-transaction mapping if the block or tx gets deleted.
       table.primary(['address_id', 'block_number', 'tx_index', 'trace_id']);
     })
     .createTable('abi_spec', (table) => {
-      table.integer('abi_address_id').unsigned().references('id').inTable('address').notNullable().onDelete('CASCADE');
+      table.integer('abi_address_id').unsigned().references('id').inTable('address').notNullable()
+        .onDelete('RESTRICT'); // prevent address deletion if we have its ABI spec
       table.string('fn_name').notNullable();
       table.string('arg_name').notNullable();
       table.string('arg_type').notNullable();
@@ -67,7 +71,8 @@ exports.up = (knex, Promise) => {
       table.string('fn_name').notNullable();
       table.integer('arg_index').unsigned().notNullable();
       table.text('arg_value');
-      table.foreign(['block_number', 'tx_index', 'trace_id']).references(['block_number', 'tx_index', 'trace_id']).inTable('transaction').onDelete('CASCADE');
+      table.foreign(['block_number', 'tx_index', 'trace_id']).references(['block_number', 'tx_index', 'trace_id']).inTable('transaction')
+        .onDelete('CASCADE'); // If a block or tx is deleted, delete its input data.
       // it is ok to use this next set of 3 as a compound foreign key.
       // in the event of input data that we do not know, we will set the fields as:
       //    abi_address_id: ${tx 'to' address}
@@ -75,7 +80,8 @@ exports.up = (knex, Promise) => {
       //    arg_index: null
       //    data: garbled
       // what if the input data comes in but it's not a function call?
-      table.foreign(['abi_address_id', 'fn_name', 'arg_index']).references(['abi_address_id', 'fn_name', 'arg_index']).inTable('abi_spec').onDelete('CASCADE');
+      table.foreign(['abi_address_id', 'fn_name', 'arg_index']).references(['abi_address_id', 'fn_name', 'arg_index']).inTable('abi_spec')
+        .onDelete('RESTRICT'); // Prevent ABI deletion if input data exists for it.
       table.primary(['block_number', 'tx_index', 'trace_id', 'abi_address_id', 'fn_name', 'arg_index']);
     });
 };
