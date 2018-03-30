10aa4bd034eca80b67fe3871bdea0377a30938fd
emazurek <changes@gmail.com>
2018-03-30 11:48:31 -0400

    API v0: comment out POST transaction route

--- api/v0/src/server/routes/transactions.js
+++ api/v0/src/server/routes/transactions.js
@@ -4,39 +4,39 @@ const knex = require('../db/connection');
 const authHelpers = require('../auth/_helpers');
 
 router.get('/', (req, res, next) => {
-  knex('transactions').select('*').then((transactions) => {
+  knex('transaction').select('*').then((transactions) => {
     res.status(200).json({status: 'success', data: transactions});
   }).catch((err) => {
     res.status(500).json({status: 'error', data: err});
   });
 });
 
-router.post('/',
-  authHelpers.ensureAuthenticated,
-  (req, res, next) => {
-  return knex.transaction((t) => {
-    const toInsert = req.body.map(obj => {
-      return {
-        dateTime: obj.dateTime,
-        blockNumber: obj.blockNumber,
-        transactionIndex: obj.transactionIndex,
-        from: obj.from,
-        to: obj.to,
-        value: obj.value,
-        gasCost: obj.gasCost,
-        isError: obj.isError,
-        isFinalized: obj.isFinalized,
-        articulated: obj.articulated
-      }
-    });
-    return Promise.all(toInsert.map((obj) => {
-      return t.insert(obj).into('transactions');
-    }));
-  }).then((transactions) => {
-    res.status(201).json({status: 'success', data: transactions});
-  }).catch((err) => {
-    res.status(500).json({status: 'error', data: err});
-  });
-});
+// router.post('/',
+//   authHelpers.ensureAuthenticated,
+//   (req, res, next) => {
+//   return knex.transaction((t) => {
+//     const toInsert = req.body.map(obj => {
+//       return {
+//         dateTime: obj.dateTime,
+//         blockNumber: obj.blockNumber,
+//         transactionIndex: obj.transactionIndex,
+//         from: obj.from,
+//         to: obj.to,
+//         value: obj.value,
+//         gasCost: obj.gasCost,
+//         isError: obj.isError,
+//         isFinalized: obj.isFinalized,
+//         articulated: obj.articulated
+//       }
+//     });
+//     return Promise.all(toInsert.map((obj) => {
+//       return t.insert(obj).into('transactions');
+//     }));
+//   }).then((transactions) => {
+//     res.status(201).json({status: 'success', data: transactions});
+//   }).catch((err) => {
+//     res.status(500).json({status: 'error', data: err});
+//   });
+// });
 
 module.exports = router;
--- api/v0/test/integration/routes.transactions.test.js
+++ api/v0/test/integration/routes.transactions.test.js
@@ -50,109 +50,109 @@ describe('routes : transactions', () => {
     });
   });
 
-  const exampleObj =
-    {
-      dateTime: '1980-10-3 12:10:11',
-      blockNumber: 40381503,
-      transactionIndex: 1,
-      from: '0x06012c8cf97bead5deae237070f9587f8e7a266d',
-      to: '0x04w32cncgf7bead5deac2370m0f5587d8e7a2123',
-      value: 1.1,
-      gasCost: 0.59135831,
-      isError: false,
-      isFinalized: false,
-      articulated: 'execute("random stuff", "other random", 301)'
-    }
+  // const exampleObj =
+  //   {
+  //     dateTime: '1980-10-3 12:10:11',
+  //     blockNumber: 40381503,
+  //     transactionIndex: 1,
+  //     from: '0x06012c8cf97bead5deae237070f9587f8e7a266d',
+  //     to: '0x04w32cncgf7bead5deac2370m0f5587d8e7a2123',
+  //     value: 1.1,
+  //     gasCost: 0.59135831,
+  //     isError: false,
+  //     isFinalized: false,
+  //     articulated: 'execute("random stuff", "other random", 301)'
+  //   }
 
-  describe('POST /api/v0/transactions', () => {
-    it('should fail if user not logged in', (done) => {
-      chai.request(server).post('/api/v0/transactions')
-      .send([exampleObj])
-      .end((err, res) => {
-        should.exist(err);
-        res.status.should.equal(400);
-        res.type.should.equal('application/json');
-        res.body.status.should.eql('Not logged in');
-        done();
-      })
-    });
-
-    it('should respond with a success message and an id when a single transaction is added', (done) => {
-      chai.request(server).post('/api/v0/auth/login').send({username: 'realuser', password: 'doingstuff'}).end((err, res) => {
-        should.not.exist(err);
-        chai.request(server).post('/api/v0/transactions').set('authorization', 'Bearer ' + res.body.token).send([
-          exampleObj
-        ]).end((err, res) => {
-
-          // there should be no errors
-          should.not.exist(err);
-          // there should be a 201 status code
-          // (indicating that something was "created")
-          res.status.should.equal(201);
-          // the response should be JSON
-          res.type.should.equal('application/json');
-          // the JSON response body should have a
-          // key-value pair of {"status": "success"}
-          res.body.status.should.eql('success');
-          // the JSON response body should have a
-          // key-value pair of {"data": 1 user object}
-          res.body.data.length.should.eql(1);
-          // the JSON response body should be a number
-          res.body.data[0].map(id => id.should.be.a('number'));
-          done();
-        });
-      });
-    });
-    it('should respond with a success message and an array of ids when multiple transactions are added', (done) => {
-      chai.request(server).post('/api/v0/auth/login').send({username: 'realuser', password: 'doingstuff'}).end((err, res) => {
-        should.not.exist(err);
-        chai.request(server).post('/api/v0/transactions').set('authorization', 'Bearer ' + res.body.token).send(Array(5).fill(exampleObj)).end((err, res) => {
-          // there should be no errors
-          should.not.exist(err);
-          // there should be a 201 status code
-          // (indicating that something was "created")
-          res.status.should.equal(201);
-          // the response should be JSON
-          res.type.should.equal('application/json');
-          // the JSON response body should have a
-          // key-value pair of {"status": "success"}
-          res.body.status.should.eql('success');
-          // the JSON response body should have a
-          // key-value pair of {"data": 1 user object}
-          res.body.data.length.should.eql(5);
-          // the JSON response body should be an array of numbers
-          res.body.data[0].map(id => id.should.be.a('number'));
-          done();
-        });
-      });
-    });
-    it('should reject all transactions when 1 is wrong', (done) => {
-      chai.request(server).post('/api/v0/auth/login').send({username: 'realuser', password: 'doingstuff'}).end((err, res) => {
-        chai.request(server).post('/api/v0/transactions')
-          .set('authorization', 'Bearer ' + res.body.token)
-          .send(Array(5).fill(exampleObj).concat({notRightAtAll: 'this is not a valid field', blockNumber: 'q'})).end((err, res) => {
-          // there should be an error
-          should.exist(err);
-          chai.request(server).get('/api/v0/transactions').end((err, res) => {
-            // there should be no errors
-            should.not.exist(err);
-            // there should be a 200 status code
-            res.status.should.equal(200);
-            // the response should be JSON
-            res.type.should.equal('application/json');
-            // the JSON response body should have a
-            // key-value pair of {"status": "success"}
-            res.body.status.should.eql('success');
-            // the JSON response body should have a
-            // key-value pair of {"data": [10 transaction objects]}
-            res.body.data.length.should.eql(10);
-            // the first object in the data array should
-            // have the right keys
-            res.body.data[0].should.include.keys('id', 'dateTime', 'blockNumber', 'transactionIndex', 'from', 'to', 'value', 'gasCost', 'isError', 'isFinalized', 'articulated', 'created_at',);
-            done();
-          });
-        });
-      });
-    });
-  });
+  // describe('POST /api/v0/transactions', () => {
+  //   it('should fail if user not logged in', (done) => {
+  //     chai.request(server).post('/api/v0/transactions')
+  //     .send([exampleObj])
+  //     .end((err, res) => {
+  //       should.exist(err);
+  //       res.status.should.equal(400);
+  //       res.type.should.equal('application/json');
+  //       res.body.status.should.eql('Not logged in');
+  //       done();
+  //     })
+  //   });
+  //
+  //   it('should respond with a success message and an id when a single transaction is added', (done) => {
+  //     chai.request(server).post('/api/v0/auth/login').send({username: 'realuser', password: 'doingstuff'}).end((err, res) => {
+  //       should.not.exist(err);
+  //       chai.request(server).post('/api/v0/transactions').set('authorization', 'Bearer ' + res.body.token).send([
+  //         exampleObj
+  //       ]).end((err, res) => {
+  //
+  //         // there should be no errors
+  //         should.not.exist(err);
+  //         // there should be a 201 status code
+  //         // (indicating that something was "created")
+  //         res.status.should.equal(201);
+  //         // the response should be JSON
+  //         res.type.should.equal('application/json');
+  //         // the JSON response body should have a
+  //         // key-value pair of {"status": "success"}
+  //         res.body.status.should.eql('success');
+  //         // the JSON response body should have a
+  //         // key-value pair of {"data": 1 user object}
+  //         res.body.data.length.should.eql(1);
+  //         // the JSON response body should be a number
+  //         res.body.data[0].map(id => id.should.be.a('number'));
+  //         done();
+  //       });
+  //     });
+  //   });
+  //   it('should respond with a success message and an array of ids when multiple transactions are added', (done) => {
+  //     chai.request(server).post('/api/v0/auth/login').send({username: 'realuser', password: 'doingstuff'}).end((err, res) => {
+  //       should.not.exist(err);
+  //       chai.request(server).post('/api/v0/transactions').set('authorization', 'Bearer ' + res.body.token).send(Array(5).fill(exampleObj)).end((err, res) => {
+  //         // there should be no errors
+  //         should.not.exist(err);
+  //         // there should be a 201 status code
+  //         // (indicating that something was "created")
+  //         res.status.should.equal(201);
+  //         // the response should be JSON
+  //         res.type.should.equal('application/json');
+  //         // the JSON response body should have a
+  //         // key-value pair of {"status": "success"}
+  //         res.body.status.should.eql('success');
+  //         // the JSON response body should have a
+  //         // key-value pair of {"data": 1 user object}
+  //         res.body.data.length.should.eql(5);
+  //         // the JSON response body should be an array of numbers
+  //         res.body.data[0].map(id => id.should.be.a('number'));
+  //         done();
+  //       });
+  //     });
+  //   });
+  //   it('should reject all transactions when 1 is wrong', (done) => {
+  //     chai.request(server).post('/api/v0/auth/login').send({username: 'realuser', password: 'doingstuff'}).end((err, res) => {
+  //       chai.request(server).post('/api/v0/transactions')
+  //         .set('authorization', 'Bearer ' + res.body.token)
+  //         .send(Array(5).fill(exampleObj).concat({notRightAtAll: 'this is not a valid field', blockNumber: 'q'})).end((err, res) => {
+  //         // there should be an error
+  //         should.exist(err);
+  //         chai.request(server).get('/api/v0/transactions').end((err, res) => {
+  //           // there should be no errors
+  //           should.not.exist(err);
+  //           // there should be a 200 status code
+  //           res.status.should.equal(200);
+  //           // the response should be JSON
+  //           res.type.should.equal('application/json');
+  //           // the JSON response body should have a
+  //           // key-value pair of {"status": "success"}
+  //           res.body.status.should.eql('success');
+  //           // the JSON response body should have a
+  //           // key-value pair of {"data": [10 transaction objects]}
+  //           res.body.data.length.should.eql(10);
+  //           // the first object in the data array should
+  //           // have the right keys
+  //           res.body.data[0].should.include.keys('id', 'dateTime', 'blockNumber', 'transactionIndex', 'from', 'to', 'value', 'gasCost', 'isError', 'isFinalized', 'articulated', 'created_at',);
+  //           done();
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
 });
