8127362fba0ead55c0227725d03e48c324cb3aad
emazurek <changes@gmail.com>
2018-03-30 18:33:28 -0400

    add explicit transaction colnames to route

--- api/v0/src/server/routes/transactions.js
+++ api/v0/src/server/routes/transactions.js
@@ -3,14 +3,29 @@ const router = express.Router();
 const knex = require('../db/connection');
 const authHelpers = require('../auth/_helpers');
 
+const selectCols = [
+  knex.raw('LOWER(CONCAT("0x",HEX(mt.address))) AS monitor_address'),
+  'b.timestamp AS block_timestamp',
+  't.block_number AS block_number',
+  't.tx_index AS tx_index',
+  't.trace_id AS trace_id',
+  knex.raw('LOWER(CONCAT("0x",HEX(from_address))) AS from_address'),
+  knex.raw('LOWER(CONCAT("0x",HEX(to_address))) AS to_address'),
+  't.value_wei',
+  't.gas_used',
+  't.gas_price',
+  't.input_articulated'
+]
+
 router.get('/monitor/:monitorAddress', (req, res, next) => {
-  knex('transaction').select('*')
-    .join('monitor_transaction', function() {
-      this.on('transaction.block_number', '=', 'monitor_transaction.block_number')
-      .andOn('transaction.tx_index', '=', 'monitor_transaction.tx_index')
-      .andOn('transaction.trace_id', '=', 'monitor_transaction.trace_id')
+  knex('transaction AS t').select(selectCols)
+    .join('monitor_transaction AS mt', function() {
+      this.on('t.block_number', '=', 'mt.block_number')
+      .andOn('t.tx_index', '=', 'mt.tx_index')
+      .andOn('t.trace_id', '=', 'mt.trace_id')
     })
-    .whereRaw(`monitor_transaction.address = UNHEX('${req.params.monitorAddress.substring(2)}')`)
+    .join('block AS b', 't.block_number', 'b.block_number')
+    .whereRaw(`mt.address = UNHEX('${req.params.monitorAddress.substring(2)}')`)
     .then((transactions) => {
       res.status(200).json({status: 'success', data: transactions});
     }).catch((err) => {
