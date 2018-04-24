5a55cc09102e39de0f99136fa9fdc64dcacebd91
emazurek <changes@gmail.com>
2018-04-24 18:05:40 -0400

    add basic monitor group ui route

--- api/v1/src/server/routes/ui.js
+++ api/v1/src/server/routes/ui.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/connection');
 
+router.get('/monitor-groups', (req, res, next) => {
+  knex('monitor_group AS mg')
+    .select(['mg.nickname AS groupName', 'm.monitorAddress', 'm.nickname AS monitorName', 'm.firstBlock'])
+    .join('monitor_monitor_group AS mmg', 'mg.monitorGroupID', 'mmg.monitorGroupID')
+    .join('monitor AS m', 'mmg.monitorAddress', 'm.monitorAddress')
+  .then((monitorGroups) => {
+    res.status(200).json({status: 'success', data: {
+        monitorGroups: monitorGroups
+      }});
+    }).catch((err) => {
+      res.status(500).json({status: 'error', data: err});
+    });
+});
+
 router.get('/', (req, res, next) => {
   knex('monitor_transaction AS mt')
     .distinct('monitorAddress')
@@ -14,5 +28,14 @@ router.get('/', (req, res, next) => {
     res.status(500).json({status: 'error', data: err});
   });
 });
+
+// knex('transaction AS t').select(selectCols)
+//   .join('monitor_transaction AS mt', function() {
+//     this.on('t.blockNumber', '=', 'mt.blockNumber')
+//     .andOn('t.transID', '=', 'mt.transID')
+//     .andOn('t.traceID', '=', 'mt.traceID')
+//   })
+
+
 
 module.exports = router;
