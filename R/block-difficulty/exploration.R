1b428a20e0c8cccb887d9a31af4faf12ef188a84
wildmolasses <changes@gmail.com>
2018-11-07 13:55:42 -0500

    add block time stats

--- R/block-difficulty/exploration.R
+++ R/block-difficulty/exploration.R
@@ -3,26 +3,9 @@ require(tidyverse)
 hashrate <- read_csv('average-hashrate-of-the-ethereum-network.csv',
                      col_names = c('date', 'hashrate'),
                      col_types = '??',
-                     skip = 1) 
-<<<<<<< HEAD
-difficulty <- read_csv('difficulty-generated-1a.csv') %>% filter(block.number > 6500000)
-difficulty <-
-  mutate(period = floor(difficulty["block.number"] / 100000)
-         
-         %>%
-  mutate(e = 2 ^ period)
-                    
-
-e.per.25000 <- period %>%
-  mutate(e = power(2,period)) %>%
-  group_by(e)
-
-#%>%
-#  filter(block.number > 3000000);
-=======
+                     skip = 1)
 
 difficulty <- read_csv('difficulty-generated-1a.csv')
->>>>>>> c283aaaf4405675c324ac1369dcc2613df2e03d4
 
 bomb <- as_tibble(data.frame(block.number = c(0:7000000))) %>%
   mutate(bomb = ifelse(block.number >= 4375000, 2^(abs((block.number - 3000000 + 1)/100000)-2), 2^(abs((block.number + 1)/100000)-2)))
@@ -57,7 +40,7 @@ avg.difficulty %>%
 
 avg.difficulty %>%
   left_join(bomb.avg, by = 'block.bin') %>%
-  ggplot(aes(x=block.bin)) + 
+  ggplot(aes(x=block.bin)) +
   geom_line(aes(y=mean.difficulty, color='difficulty')) +
   geom_line(aes(y=mean.bomb, color='bomb'))
 
@@ -80,7 +63,7 @@ difficulty %>%
   summarize(mean.block.time.elapsed = mean(block.time.elapsed)) %>%
   ggplot(aes(x=block.bin, y=mean.block.time.elapsed)) +
   geom_line()
-  
+
 difficulty %>%
   mutate(lag.timestamp = lag(timestamp)) %>%
   mutate(block.time.elapsed = timestamp - lag.timestamp) %>%
@@ -132,4 +115,9 @@ difficulty %>%
   geom_line() +
   facet_wrap(facets = 'vars', scales = 'free', ncol = 2)
 
-
+block.time.stats <- difficulty %>%
+  mutate(span = (timestamp - lag(timestamp))) %>%
+  filter(!is.na(span)) %>%
+  summarize(mean = mean(span),
+            median = median(span),
+            sd = sd(span))
