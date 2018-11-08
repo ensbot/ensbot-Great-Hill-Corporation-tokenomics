8f7a26ea924500c893f56cb8542112873fb71b42
Thomas Jay Rush <jrush@quickblocks.io>
2018-11-07 23:37:54 -0500

    Adding .tjr R code

---
+++ R/block-difficulty/exploration.tjr.R
@@ -0,0 +1,97 @@
+require(tidyverse)
+require(scales)
+require(dplyr)
+require(magrittr)
+
+homestead.block <- 1150000
+byzantium.block <- 4370000
+bin_size        <- 5000
+period_size     <- 100000
+sample_size     <- 20000
+
+hashrate <- read_csv('average-hashrate-of-the-ethereum-network.csv',
+                     col_names = c('date', 'hashrate'),
+                     col_types = '??',
+                     skip = 1)
+hashrate %>%
+  ggplot(aes(x=date, y=hashrate, color=hashrate)) +
+  scale_color_gradientn(colours = rainbow(10), labels = comma) +
+  geom_line()
+
+difficulty <- read_csv('difficulty-generated-1a.csv') %>%
+  mutate(block.bin = floor(block.number / bin_size) * bin_size) %>%
+  mutate(fake.block = ifelse(block.number >= byzantium.block, block.number - 3000000, block.number) + 1) %>%
+  mutate(period = floor(fake.block / period_size)) %>%
+  mutate(bomb = 2 ^ period) %>%
+  mutate(parent.difficulty = lag(difficulty)) %>%
+  mutate(diff.delta = difficulty - parent.difficulty) %>%
+  mutate(pct.delta.to.difficulty = diff.delta / difficulty) %>%
+  mutate(parent.ts = lag(timestamp)) %>%
+  mutate(ts.delta = timestamp - parent.ts)
+current.block <- difficulty$block.number %>% tail(1)
+current.bomb <- max(difficulty$bomb)
+
+debug = 1
+if (debug) {
+  difficulty %>%
+    sample_n(sample_size) %>%
+    group_by(block.bin) %>%
+    ggplot(aes(x=block.number)) +
+    geom_line(aes(y=diff.delta, color='diff.pct')) +
+    geom_line(aes(y=bomb, color='bomb'))
+}
+
+difficulty %>%
+  group_by(block.bin) %>%
+  summarize(sum.diff.delta = sum(diff.delta), na.rm=T) %>%
+  ggplot(aes(x=block.bin, y=sum.diff.delta)) +
+  geom_line()
+
+difficulty %>%
+  group_by(block.bin) %>%
+  summarize(sum.diff.delta = sum(diff.delta, na.rm=T), mean.ts.delta = mean(ts.delta, na.rm=T)) %>%
+  gather(key = vars, value = val, -block.bin) %>%
+  ggplot(aes(x=block.bin, y = val)) +
+  geom_line() +
+  facet_wrap(facets = 'vars', scales = 'free', ncol = 1)
+
+difficulty %>%
+  group_by(block.bin) %>%
+  summarize(sum.difficulty = sum(difficulty), sum.diff.delta = sum(diff.delta, na.rm=T), mean.ts.delta = mean(ts.delta, na.rm=T)) %>%
+  mutate(percent.delta = sum.diff.delta / sum.difficulty) %>%
+  gather(key = vars, value = val, -block.bin) %>%
+  ggplot(aes(x=block.bin, y = val)) +
+  geom_line() +
+  facet_wrap(facets = 'vars', scales = 'free', ncol = 2)
+
+difficulty %>%
+  group_by(block.bin) %>%
+  summarize(sd = sd(ts.delta, na.rm=T)) %>%
+  ggplot(aes(x=block.bin, y=sd)) +
+  geom_line()
+
+difficulty %>%
+  sample_n(sample_size) %>%
+  ggplot(aes(y=pct.delta.to.difficulty, x = ts.delta, color = block.number)) +
+  geom_point(size = 1.5) +
+  scale_color_gradientn(colours = rainbow(10), labels = comma) +
+  scale_x_continuous(breaks = -1:8 * 60)
+
+point_size = 1.5
+difficulty %>%
+  filter(block.number > homestead.block) %>%
+  mutate(era = ifelse(block.number <= byzantium.block, 'before byzantium', 'post byzantium')) %>%
+  sample_n(sample_size) %>%
+  ggplot(aes(y = pct.delta.to.difficulty, x = period, color=block.number)) +
+  scale_colour_gradient2(low = "red", mid = "green", high = "blue", midpoint = byzantium.block, space = "Lab", na.value = "grey50", guide = "colourbar") +
+  geom_point(size = point_size) + 
+  facet_wrap(facets = 'era', nrow = 2) +
+  geom_vline(xintercept = 35)
+
+difficulty %>%
+  filter(block.number > homestead.block) %>%
+  sample_n(sample_size) %>%
+  ggplot(aes(y = pct.delta.to.difficulty, x = period, color=block.number)) +
+  scale_colour_gradient2(low = "red", mid = "green", high = "blue", midpoint = byzantium.block, space = "Lab", na.value = "grey50", guide = "colourbar") +
+  geom_point(size = point_size) + 
+  geom_vline(xintercept = 35)
