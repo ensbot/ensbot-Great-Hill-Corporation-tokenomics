3404c574a61a9852d0decd91bcf2e513e3eeacf0
Thomas Jay Rush <jrush@greathill.com>
2018-12-29 18:51:50 +0100

    Cleaning up trj exploration:

--- R/block-difficulty/exploration.tjr.R
+++ R/block-difficulty/exploration.tjr.R
@@ -5,29 +5,43 @@ require(magrittr)
 
 homestead.block <- 1150000
 byzantium.block <- 4370000
-bin_size        <- 2000
+bin_size        <- 200
 period_size     <- 100000
-sample_size     <- 5000
+sample_size     <- 50000
 
 difficulty <- read_csv('difficulty-generated-1a.csv') %>%
+  filter(block.number > homestead.block) %>%
   mutate(block.bin = floor(block.number / bin_size) * bin_size) %>%
   mutate(fake.block = ifelse(block.number >= byzantium.block, block.number - 3000000, block.number) + 1) %>%
   mutate(period = floor(fake.block / period_size)) %>%
   mutate(bomb = 2 ^ period) %>%
+
   mutate(parent.difficulty = lag(difficulty)) %>%
   mutate(parent.ts = lag(timestamp)) %>%
+  
   mutate(diff.delta = difficulty - parent.difficulty) %>%
-  mutate(sensitivity = diff.delta / difficulty) %>%
-  mutate(ts.delta = timestamp - parent.ts)
+  mutate(ts.delta = timestamp - parent.ts) %>%
+
+  mutate(diff.sensitivity = diff.delta / difficulty) %>%
+  mutate(ts.sensitivity = ts.delta / timestamp)
+
 current.block <- difficulty$block.number %>% tail(1)
 current.bomb <- max(difficulty$bomb)
 
+# difficulty delta vs. block number
 difficulty %>%
-#  sample_n(sample_size) %>%
+  sample_n(sample_size) %>%
   group_by(block.bin) %>%
   ggplot(aes(x=block.number)) +
-  geom_line(aes(y=diff.delta, color='diff.pct')) +
+  geom_line(aes(y=diff.delta, color='diff.delta')) +
   geom_line(aes(y=bomb, color='bomb'))
+
+# difficulty sensitivity vs. block number
+difficulty %>%
+  sample_n(sample_size) %>%
+  group_by(block.bin) %>%
+  ggplot(aes(x=block.number)) +
+  geom_line(aes(y=diff.sensitivity, color='diff.sensitivity'))
 
 difficulty %>%
   group_by(block.bin) %>%
@@ -58,28 +72,26 @@ difficulty %>%
   ggplot(aes(x=block.bin, y=sd)) +
   geom_line()
 
+point_size = 1.0
 difficulty %>%
   sample_n(sample_size) %>%
-  ggplot(aes(y=sensitivity, x = ts.delta, color = block.number)) +
-  geom_point(size = 1.5) +
+  ggplot(aes(y=diff.sensitivity, x = ts.delta, color = block.number)) +
+  geom_point(size = point_size) + 
   scale_color_gradientn(colours = rainbow(10), labels = comma) +
   scale_x_continuous(breaks = -1:8 * 60)
 
-point_size = 1.5
 difficulty %>%
-  filter(block.number > homestead.block) %>%
   mutate(era = ifelse(block.number <= byzantium.block, 'before byzantium', 'post byzantium')) %>%
   sample_n(sample_size) %>%
-  ggplot(aes(y = sensitivity, x = period, color=block.number)) +
+  ggplot(aes(y = diff.sensitivity, x = period, color=block.number)) +
   scale_colour_gradient2(low = "red", mid = "green", high = "blue", midpoint = byzantium.block, space = "Lab", na.value = "grey50", guide = "colourbar") +
   geom_point(size = point_size) + 
   facet_wrap(facets = 'era', nrow = 2) +
   geom_vline(xintercept = 39)
 
 difficulty %>%
-  filter(block.number > homestead.block) %>%
   sample_n(sample_size) %>%
-  ggplot(aes(y = sensitivity, x = period, color=block.number)) +
+  ggplot(aes(y = diff.sensitivity, x = period, color=block.number)) +
   scale_colour_gradient2(low = "red", mid = "green", high = "blue", midpoint = byzantium.block, space = "Lab", na.value = "grey50", guide = "colourbar") +
   geom_point(size = point_size) + 
   geom_vline(xintercept = 39)
