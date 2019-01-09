df57b462b884ed98e23c6c1b4b73aee50c2bbc54
Thomas Jay Rush <jrush@quickblocks.io>
2019-01-08 21:13:34 -0500

    Adding code

--- R/fiat-contract/transactions-by-day.R
+++ R/fiat-contract/transactions-by-day.R
@@ -1,8 +1,32 @@
 library(readr)
+require(tidyverse)
+require(scales)
+require(dplyr)
+require(magrittr)
 
 transactions <- read_csv("transactions.csv")
 
-#block.txs <- data.frame(transactions$blocknumber, transactions$transactionindex)
-block.dates <- data.frame(transactions$blocknumber, transactions$date)
+block.dates <- data.frame(transactions$blocknumber, as.Date(transactions$date))
 
+block.dates %>% head() %>% View()
+
+block.dates <-
+  block.dates %>%
+  mutate(date = as.Date(date)) %>%
+  group_by(date) %>%
+  count(date)
+
+block.dates <- block.dates %>%
+  filter(n != 222) %>%
+  filter(n != 286)
+  
+block.dates
+
+block.dates %>%
+  ggplot(aes(x=date, y = n)) +
+  geom_line()
+
+
+block.dates <- block.date %>% summarize()
+block.dates
 plot(block.dates)
